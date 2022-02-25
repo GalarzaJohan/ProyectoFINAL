5745dbde86f2fee4cb14a6e0c6aeaaf8affa0bc5
GalarzaJohan <100308587+GalarzaJohan@users.noreply.github.com>
2022-02-24 20:00:37 -0500

    primer commit

---
+++ src/proyectofinal/FXMLEditorController.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectofinal;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author familia
+ */
+public class FXMLEditorController implements Initializable {
+    
+      @FXML
+    private ComboBox<?> CvboxEditor;
+
+    @FXML
+    private TextField TxtNombre;
+
+    @FXML
+    private TextField TxtPrimera;
+
+    @FXML
+    private TextField TxtPersonaje;
+
+    @FXML
+    private TextField TxtEgo;
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private Button Btnguardar;
+
+    @FXML
+    private Button btnCerrar;
+
+    @FXML
+    void Cerrar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void f2eeee(ActionEvent event) {
+
+    }
+
+    @FXML
+    void f8f3f3(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ebeaea(ActionEvent event) {
+
+    }
+
+    @FXML
+    void f2ebeb(ActionEvent event) {
+
+    }
+
+    @FXML
+    void f5f0f0(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Guardar(ActionEvent event) {
+
+    }
+
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
